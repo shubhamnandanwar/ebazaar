056627899b7b2b9224936484ea958f6209ce1ca9
ShubhamNandanwar <shubhamnandanwar9776@gmail.com>
2017-07-14 19:23:29 +0530

    Greater than zero validation added in prize attribute

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,5 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+  validates :price, :numericality => { :greater_than_or_equal_to => 0 }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :price, :numericality => { :greater_than_or_equal_to => 0 }
 end
